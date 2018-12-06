ce019c0d1a76bede8baf858c20730c5f7e5be188
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-06 21:06:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,83 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("517751305868017674");
+const mohamed= member.guild.channels.get("520235963297824778");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(`**WelcomeTo__Txe__ :Txe: Server Enjoooy :dove:  .**`), 4000)        
 }
 });
+
+var adminprefix = "&";
+
+const developers = ["378982385779343360","323888904602124299"]
+
+client.on('message', message => {
+
+    var argresult = message.content.split(` `).slice(1).join(' ');
+
+      if (!developers.includes(message.author.id)) return;
+
+      
+
+  if (message.content.startsWith(adminprefix + 'setg')) {
+
+    client.user.setGame(argresult);
+
+      message.channel.send(`**تم تغيير حالتك الى** **${argresult}**`)
+
+  } else 
+
+     if (message.content === (adminprefix + "leave")) {
+
+    message.guild.leave(); 
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'setw')) {
+
+  client.user.setActivity(argresult, {type:'WATCHING'});
+
+      message.channel.send(`**تم تغيير الواتشنق الى** **${argresult}**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'setl')) {
+
+  client.user.setActivity(argresult , {type:'LISTENING'});
+
+      message.channel.send(`**تم تغير اللستنق الى ** **${argresult}**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'sets')) {
+
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+
+      message.channel.send(`**تم تغيير الستريمنق الى** **${argresult}**`)
+
+  }
+
+  if (message.content.startsWith(adminprefix + 'setname')) {
+
+  client.user.setUsername(argresult).then
+
+      message.channel.send(`**تم تغيير اسمك الى ** ..**${argresult}** `)
+
+} else
+
+if (message.content.startsWith(adminprefix + 'setava')) {
+
+  client.user.setAvatar(argresult);
+
+    message.channel.send(`**تم تغيير صورتك الى**:**${argresult}** `);
+
+}
+
+});
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
